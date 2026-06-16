a60b73a9b1d81b32fbfd80fb73c335901e8d391f
Nathnael <nathnael.demere@gmail.com>
2026-06-16 12:13:19 +0300

    Refactor CollectionKvConverter into a sealed class and expose public implementations
    - Change `CollectionKvConverter` from an abstract namespace class to a sealed class hierarchy.
    - Replace private `_ListKvConverter` and `_SetKvConverter` with public `ListKvConverter` and `SetKvConverter` classes.
    - Remove the static `toList` and `toSet` factory methods from `CollectionKvConverter`.
    - Update `collection_converter_test.dart` to reflect the API changes by using direct class constructors.

--- packages/omni_kv/lib/src/converters/collection_converter.dart
+++ packages/omni_kv/lib/src/converters/collection_converter.dart
@@ -1,20 +1,12 @@
 import '../core/kv_converter.dart';
 
-/// A namespace for collection converters.
-abstract final class CollectionKvConverter {
-  /// Converts a List of items, applying the [elementConverter] to each item.
-  static KvConverter<List<T>?, Object?> toList<T>(
-    KvConverter<T?, Object?> elementConverter,
-  ) => _ListKvConverter<T>(elementConverter);
+sealed class CollectionKvConverter<T, S> implements KvConverter<Iterable<T>?, Object?> {
 
-  /// Converts a Set of items, safely storing them as a List in the database.
-  static KvConverter<Set<T>?, Object?> toSet<T>(
-    KvConverter<T?, Object?> elementConverter,
-  ) => _SetKvConverter<T>(elementConverter);
 }
 
-final class _ListKvConverter<T> implements KvConverter<List<T>?, Object?> {
-  const _ListKvConverter(this.elementConverter);
+/// Converts a List of items, applying the [elementConverter] to each item.
+final class ListKvConverter<T, S> implements KvConverter<List<T>?, Object?> {
+  const ListKvConverter(this.elementConverter);
 
   final KvConverter<T?, Object?> elementConverter;
 
@@ -32,8 +24,9 @@ final class _ListKvConverter<T> implements KvConverter<List<T>?, Object?> {
   }
 }
 
-final class _SetKvConverter<T> implements KvConverter<Set<T>?, Object?> {
-  const _SetKvConverter(this.elementConverter);
+/// Converts a Set of items, safely storing them as a List in the database.
+final class SetKvConverter<T, S> implements KvConverter<Set<T>?, Object?> {
+  const SetKvConverter(this.elementConverter);
 
   final KvConverter<T?, Object?> elementConverter;
 
